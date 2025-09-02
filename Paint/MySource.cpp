88dfa1b2be58abfdb53ef38cc6167cb0d685e74e
user <user@DESKTOP-V6A4RI2>
2025-09-02 14:25:54 +0900

    초기 커밋.

---
+++ Paint/MySource.cpp
@@ -0,0 +1,22 @@
+#include "MySource.h"
+
+HWND InitMainWindowSet(HINSTANCE hInstance, WNDPROC WndProc, const WCHAR* name) {
+
+	// 윈도우 창 설정 및 적용하기
+	WNDCLASSEXW wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = NULL;
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = name;
+	wcex.hIconSm = NULL;
+	RegisterClassExW(&wcex);
+
+	return CreateWindowW(name, name, WS_MAXIMIZE|WS_SYSMENU, 100, 100, 750, 750, nullptr, nullptr, hInstance, nullptr);
+}
---
+++ Paint/MySource.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "framework.h"
+#include "Paint.h"
+
+HWND InitMainWindowSet(HINSTANCE hInstance, WNDPROC WndProc, const WCHAR* name);
